5c8ad4437a76ec16388f054452dd234f3af27156
yara121 <yara767@gmail.com>
2023-06-03 18:03:57 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '37afc7a4-a9a9-4a92-a7f8-109c2ee86af4';
+export const MODAL_CLOSE_SEC = 2.5;
